59da7d03b6b698fd92104e1d22a89525da593abf
Eric <ericspsantos@gmail.com>
2023-10-20 00:02:14 -0300

    atualizando filtros

--- app/src/components/Usuarios/Filtros.js
+++ app/src/components/Usuarios/Filtros.js
@@ -1,50 +1,99 @@
-"use client";
+"use client"
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Select from "react-select";
 import "@/components/Usuarios/css/Filtros.css";
-export default function Filtros() {
-  const [selectedOption, setSelectedOption] = useState("filtros");
-  const router = useRouter();
-  useEffect(() => {
-    const handleOptionChange = e => {
-      const selectedValue = e.target.value;
-      setSelectedOption(selectedValue);
 
-      if (selectedValue === "Hospitais") {
+export default function Filtros() {
+  const router = useRouter();
+  const [selectedFiltro, setSelectedFiltro] = useState(null);
+  const [selectedEspecialidade, setSelectedEspecialidade] = useState(null);
+  const [especialidades, setEspecialidades] = useState([]);
+  
+  const handleFiltroChange = (selectedOption) => {
+    setSelectedFiltro(selectedOption);
+    
+    if (selectedOption) {
+      if (selectedOption.value === "Hospitais") {
         router.push("/hospitais");
-      } else if (selectedValue === "Clinicas") {
+      } else if (selectedOption.value === "Clínicas") {
         router.push("/clinicas");
       }
-    };
-
-    const selectElement = document.querySelector(".select-element-filtros");
-
-    if (selectElement) {
-      selectElement.addEventListener("change", handleOptionChange);
     }
+  };
 
-    return () => {
-      if (selectElement) {
-        selectElement.removeEventListener("change", handleOptionChange);
-      }
-    };
+  const handleEspecialidadeChange = (selectedOption) => {
+    setSelectedEspecialidade(selectedOption);
+
+    if (selectedOption) {
+      // Gere a URL a partir do nome da especialidade
+      const especialidadeUrl = `/pesquisa/${selectedOption.label}`;
+      router.push(especialidadeUrl);
+    }
+  };
+  
+  useEffect(() => {
+    fetch("https://api-web-saude.vercel.app/especialidades")
+      .then((response) => response.json())
+      .then((data) => {
+        const options = data.map((especialidade) => ({
+          value: especialidade.id,
+          label: especialidade.nome,
+        }));
+        setEspecialidades(options);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar especialidades:", error);
+      });
   }, []);
-
+  
+  const customStyles = {
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: "#00285f",
+      color:"red",
+      borderRadius: 10,
+    }),
+    option: (styles, { isFocused, isSelected }) => ({
+      ...styles,
+      backgroundColor: isSelected ? "blue" : isFocused ? "lightgray" : "white",
+      color: isSelected ? "white" : "black",
+    }),
+    singleValue: (styles) => ({
+      ...styles,
+      color: "white",
+    }),
+    input: (styles) => ({
+      ...styles,
+      color: "white", // Defina a cor desejada para o texto de pesquisa
+    }),
+  };
+  
   return (
     <div className="div-buttons">
       <div className="div-button">
-        <select className="select-element-filtros" value={selectedOption}>
-          <option value="filtros">Filtros</option>
-          <option value="Hospitais">Hospitais</option>
-          <option value="Clinicas">Clínicas</option>
-        </select>
+        <Select
+          className="select-element-filtros"
+          value={selectedFiltro}
+          options={[
+            { value: "Hospitais", label: "Hospitais" },
+            { value: "Clínicas", label: "Clínicas" },
+          ]}
+          onChange={handleFiltroChange}
+          styles={customStyles}
+          placeholder="Filtros"
+        />
       </div>
+      
       <div className="div-button">
-        <select className="select-element" value={selectedOption}>
-          <option value="Especialidades">Especialidades</option>
-          <option value="pediatra">Pediatra</option>
-          <option value="dermatologista">Dermatologista</option>
-        </select>
+        <Select
+          className="select-element-filtros"
+          value={selectedEspecialidade}
+          options={especialidades}
+          onChange={handleEspecialidadeChange}
+          styles={customStyles}
+          placeholder="Especialidades"
+        />
       </div>
     </div>
   );
