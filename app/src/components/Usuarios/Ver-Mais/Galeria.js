80a569f5fefdf9a1720db8f6f410258c58c20732
Eric <ericspsantos@gmail.com>
2024-03-31 20:04:24 -0300

    galeria

--- app/src/components/Usuarios/Ver-Mais/Galeria.js
+++ app/src/components/Usuarios/Ver-Mais/Galeria.js
@@ -1,41 +1,33 @@
 import React from "react";
-import { Button, Card, Row, Col } from "react-bootstrap";
 import ConsultasService from "@/services/ConsultasService";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
+import Image from "next/image";
 import "@/components/Usuarios/Ver-Mais/css/Galeria.css";
 
 export default async function Galeria({ nome }) {
   const unidadeDeSaude = await ConsultasService.pegarUnidadedeSaude(nome);
 
   return (
-    <div id="galeria" className="photo-gallery">
-      <div className="container text-center">
-        <div className="intro">
-          <h2>Galeria</h2>
-        </div>
-        <Row className="justify-content-center">
+    
+    <section className="sectionGaleria">
+       <h1 className="tituloGaleria">
+        Galeria
+       </h1>
+
+       <div className="galeria">
           {unidadeDeSaude.imagens.map((src, index) => (
-            <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
-              <Card>
-                <Card.Img
-                  src={src}
-                  alt={`Imagem ${index + 1}`}
-                  style={{ cursor: "pointer" }}
-                  variant="top"
-                  className="img-fluid imagens"
-                />
-              </Card>
-            </Col>
+
+          <div key={index} className="image">
+              <Link href={src}>
+                <Image src={src} width={200} height={200} className="imagens"/>
+              </Link>
+          </div>          
           ))}
-        </Row>
-        {unidadeDeSaude.imagens.length > 2 && (
-          <div className="text-center mt-3">
-            <Button variant="primary" className="mb-2">
-              Ver Mais
-            </Button>
-          </div>
-        )}
-      </div>
-    </div>
+       </div>
+       
+       <div className="buttonGaleria">
+        <button>Ver Mais</button>
+       </div>
+    </section>
   );
 }
